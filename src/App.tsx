25b02e6dcf7a500930aed00f531fb254d6c0dfe2
Joshua <44968962+mr-jw@users.noreply.github.com>
2026-09-23 15:49:14 +0100

    feat: configuration menu changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [playerOneUpKey, setPlayerOneUpKey] = useState("w");
-  const [playerOneDownKey, setPlayerOneDownKey] = useState("s");
+type MenuProps = {
+  keyBindings: string[];
+  setKeyBindings?: void;
+};
 
-  const [playerTwoUpKey, setPlayerTwoUpKey] = useState("p");
-  const [playerTwoDownKey, setPlayerTwoDownKey] = useState("l");
-
+function Menu( {keyBindings}: MenuProps) {
   const [menu, setMenu] = useState("main-menu");
 
   const moveToConfiguration = () => {
     setMenu("config-menu");
-    console.log(menu);
   };
 
   const moveToAbout = () => {
     setMenu("about");
-  }
+  };
 
   return (
     <>
@@ -25,53 +23,75 @@ function App() {
         <p className="title">Pong</p>
 
         {menu === "main-menu" && (
-          <div className="menu-buttons">
+          <div className="main-menu-buttons">
             <button className="menu-button">Play</button>
             <button className="menu-button" onClick={moveToConfiguration}>
               Configuration
             </button>
-            <button className="menu-button" onClick={moveToAbout}>About</button>
+            <button className="menu-button" onClick={moveToAbout}>
+              About
+            </button>
           </div>
         )}
 
         {menu === "config-menu" && (
-          <div className="menu-buttons">
+          <div className="config-menu">
             <p>Player 1:</p>
             <div className="key-binding">
-              <p>Assigned Key: {playerOneUpKey}</p>
+              <p>Up: <span className="key">{keyBindings[0]}</span></p>
               <button className="menu-button">Change</button>
             </div>
-            
-             <div className="key-binding">
-              <p>Assigned Key: {playerOneDownKey}</p>
-              <button className="menu-button">Change</button>
-            </div>
-           
-            <p>Player 2:</p>
-           
+
             <div className="key-binding">
-              <p>Assigned Key: {playerTwoUpKey}</p>
+              <p>Down: <span className="key">{keyBindings[1]}</span></p>
               <button className="menu-button">Change</button>
             </div>
 
-             <div className="key-binding">
-              <p>Assigned Key: {playerTwoDownKey}</p>
+            <p>Player 2:</p>
+
+            <div className="key-binding">
+              <p>Up: <span className="key">{keyBindings[2]}</span></p>
               <button className="menu-button">Change</button>
             </div>
 
-            <button className="menu-button" onClick={() => setMenu("main-menu")}>Back</button>
+            <div className="key-binding">
+              <p>Down: <span className="key">{keyBindings[3]}</span></p>
+              <button className="menu-button">Change</button>
+            </div>
+
+            <button
+              className="menu-button"
+              onClick={() => setMenu("main-menu")}
+            >
+              Back
+            </button>
           </div>
-
-          
         )}
 
-        {menu === 'about' && (
+        {menu === "about" && (
           <>
             <p>...</p>
-            <button className="menu-button" onClick={() => setMenu("main-menu")}>Back</button>
+            <button
+              className="menu-button"
+              onClick={() => setMenu("main-menu")}
+            >
+              Back
+            </button>
           </>
-          )}
+        )}
       </div>
+    </>
+  );
+}
+
+function App() {
+  const [keyBindings, setKeyBindings] = useState<string[]>([
+    'w', 'a', 'p', 'l'
+  ]);
+
+  return (
+    <>
+      <Menu keyBindings={keyBindings} />
     </>
   );
 }
