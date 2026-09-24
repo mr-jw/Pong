9127283789b1a61fa88858315ce91baef157e355
Joshua <44968962+mr-jw@users.noreply.github.com>
2026-09-24 14:36:00 +0100

    feat: refactoring & improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
+import type { ReactNode } from "react";
 import "./App.css";
 
-type KeyBindingProps = {
+type KeyBindingElementProps = {
+  player: number;
   text: string;
   binding: string;
   index: number;
   changeKeyBinding: (index: number, binding: string) => void;
 };
 
-function KeyBinding({
+function KeyBindingElement({
+  player,
   text,
   binding,
   index,
   changeKeyBinding,
-}: KeyBindingProps) {
+}: KeyBindingElementProps) {
   // is waiting for key?..
   const [isWaitingForKey, setIsWaitingForKey] = useState(false);
 
@@ -37,22 +40,36 @@ function KeyBinding({
   }, [isWaitingForKey]);
 
   return (
-    <div className="key-binding">
+    <div className="key-binding-container">
       <p>{text}</p>
       <button className="menu-button" onClick={handleChange}>
-        {isWaitingForKey ? 'Enter a key...' : binding}
+        {isWaitingForKey ? "Enter a key..." : binding}
       </button>
     </div>
   );
 }
 
-type MenuProps = {
-  keyBindings: string[];
+type KeyBindingData = {
+  player: number;
+  text: string;
+  binding: string;
   changeKeyBinding: (index: number, binding: string) => void;
+  index: number;
 };
 
-function Menu({ keyBindings, changeKeyBinding }: MenuProps) {
+type MenuProps = {
+  keyBindingData: KeyBindingData[];
+};
+
+function Menu({ keyBindingData }: MenuProps) {
   const [menu, setMenu] = useState("main-menu");
+
+  const playerOneComponents = keyBindingData.filter(
+    (component) => component.player === 1,
+  );
+  const playerTwoComponents = keyBindingData.filter(
+    (component) => component?.player === 2,
+  );
 
   const moveToConfiguration = () => {
     setMenu("config-menu");
@@ -82,33 +99,32 @@ function Menu({ keyBindings, changeKeyBinding }: MenuProps) {
         {menu === "config-menu" && (
           <div className="config-menu">
             <p>Player 1:</p>
-            <KeyBinding
-              text="Up:"
-              binding={keyBindings[0]}
-              changeKeyBinding={changeKeyBinding}
-              index={0}
-            />
-            <KeyBinding
-              text="Down:"
-              binding={keyBindings[1]}
-              changeKeyBinding={changeKeyBinding}
-              index={1}
-            />
+        
+            {/* render player one key binding elements */}
+            {playerOneComponents.map((data) => (
+              <KeyBindingElement
+                key={data.index}
+                player={data.player}
+                text={data.text}
+                binding={data.binding}
+                index={data.index}
+                changeKeyBinding={data.changeKeyBinding}
+              />
+            ))}
 
             <p>Player 2:</p>
 
-            <KeyBinding
-              text="Up:"
-              binding={keyBindings[2]}
-              changeKeyBinding={changeKeyBinding}
-              index={2}
-            />
-            <KeyBinding
-              text="Down:"
-              binding={keyBindings[3]}
-              changeKeyBinding={changeKeyBinding}
-              index={3}
-            />
+            {/* render player two key binding elements */}
+            {playerTwoComponents.map((data) => (
+              <KeyBindingElement
+                key={data.index}
+                player={data.player}
+                text={data.text}
+                binding={data.binding}
+                index={data.index}
+                changeKeyBinding={data.changeKeyBinding}
+              />
+            ))}
 
             <button
               className="menu-button config-back"
@@ -156,9 +172,40 @@ function App() {
     });
   };
 
+  const keyBindingData = [
+    {
+      player: 1,
+      text: "Up:",
+      binding: keyBindings[0],
+      changeKeyBinding: changeKeyBinding,
+      index: 0,
+    },
+    {
+      player: 1,
+      text: "Down:",
+      binding: keyBindings[1],
+      changeKeyBinding: changeKeyBinding,
+      index: 1,
+    },
+    {
+      player: 2,
+      text: "Up:",
+      binding: keyBindings[2],
+      changeKeyBinding: changeKeyBinding,
+      index: 2,
+    },
+    {
+      player: 2,
+      text: "Down:",
+      binding: keyBindings[3],
+      changeKeyBinding: changeKeyBinding,
+      index: 3,
+    },
+  ];
+
   return (
     <>
-      <Menu keyBindings={keyBindings} changeKeyBinding={changeKeyBinding} />
+      <Menu keyBindingData={keyBindingData} />
     </>
   );
 }
