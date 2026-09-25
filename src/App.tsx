0919ab441eec8a5030a12a3662261f5b584642bf
Joshua <44968962+mr-jw@users.noreply.github.com>
2026-09-25 22:05:17 +0100

    feat: movement constraints

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,31 @@ import { useEffect, useState } from "react";
 import { Menu } from "./Menu";
 import "./App.css";
 
-function Player( {player, bindings}: {player: 1 | 2, bindings: string[]}) {
-  const className = player === 1 ? 'playerOne' : 'playerTwo';
+function Player({ player, bindings }: { player: 1 | 2; bindings: string[] }) {
+  const className = player === 1 ? "playerOne" : "playerTwo";
 
   const [yPos, setYPos] = useState(0);
+
+  useEffect(() => {
+    console.log(yPos);
+  }, [yPos]);
 
   useEffect(() => {
     // keep track of key presses.
     let upKeyPressed = false;
     let downKeyPressed = false;
+
+    let animationFrame: number;
+
+    const areaElement = document.querySelector(".game-area");
+    const areaHeight = areaElement
+      ? window.getComputedStyle(areaElement).getPropertyValue("height")
+      : "0px";
+
+    const playerElement = document.querySelector(`.${className}`);
+    const playerHeight = playerElement
+      ? window.getComputedStyle(playerElement).getPropertyValue("height")
+      : "0px";
 
     // track keep pressing / releasing.
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -21,51 +37,57 @@ function Player( {player, bindings}: {player: 1 | 2, bindings: string[]}) {
       if (event.key === bindings[1]) {
         downKeyPressed = true;
       }
-    }
+    };
 
     const handleKeyRelease = (event: KeyboardEvent) => {
-      if(event.key === bindings[0]) {
+      if (event.key === bindings[0]) {
         upKeyPressed = false;
       }
 
       if (event.key === bindings[1]) {
         downKeyPressed = false;
       }
-    }
-
-    let animationFrame: number;
+    };
 
     const movePlayer = () => {
       if (upKeyPressed) {
-        setYPos((current) => current - 3);
+        setYPos((current) => (current > 0 ? current - 3 : current));
       }
 
       if (downKeyPressed) {
-        setYPos((current) => current + 3);
+        setYPos((current) =>
+          current < parseFloat(areaHeight) - parseFloat(playerHeight)
+            ? current + 3
+            : current,
+        );
       }
 
       animationFrame = requestAnimationFrame(movePlayer);
-    }
+    };
 
-    window.addEventListener('keydown', handleKeyDown);
-    window.addEventListener('keyup', handleKeyRelease);
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyRelease);
 
     animationFrame = requestAnimationFrame(movePlayer);
 
-    return () => { 
+    return () => {
       // cleanup.
-      window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('keyup', handleKeyRelease); 
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyRelease);
       cancelAnimationFrame(animationFrame);
     };
   }, [bindings, className]);
 
-  return (
-     <div className={className} style={{top: `${yPos}px`}}/>
-  );
+  return <div className={className} style={{ top: `${yPos}px` }} />;
 }
 
-function Game({ playerOneBindings, playerTwoBindings }: { playerOneBindings: string[]; playerTwoBindings: string[] }) {
+function Game({
+  playerOneBindings,
+  playerTwoBindings,
+}: {
+  playerOneBindings: string[];
+  playerTwoBindings: string[];
+}) {
   const [gameStarted, setGameStarted] = useState(false);
   const [playerOneScore, setPlayerOneScore] = useState(120);
   const [playerTwoScore, setPlayerTwoScore] = useState(40);
@@ -78,9 +100,9 @@ function Game({ playerOneBindings, playerTwoBindings }: { playerOneBindings: str
         <p className="score">{playerTwoScore}</p>
       </div>
       <div className="game-area">
-        <Player player={1} bindings={playerOneBindings}/>
+        <Player player={1} bindings={playerOneBindings} />
         <div className="game-area-separator"></div>
-        <Player player={2} bindings={playerTwoBindings}/>
+        <Player player={2} bindings={playerTwoBindings} />
       </div>
     </div>
   );
@@ -181,7 +203,12 @@ function App() {
         <Menu keyBindingData={keyBindingData} startGame={startGame} />
       )}
 
-      {gameStarted && <Game playerOneBindings={playerOneKeyBindings} playerTwoBindings={playerTwoKeyBindings} />}
+      {gameStarted && (
+        <Game
+          playerOneBindings={playerOneKeyBindings}
+          playerTwoBindings={playerTwoKeyBindings}
+        />
+      )}
     </div>
   );
 }
